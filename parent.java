865cf7b56594f9fc00290fcd125c49cfbea34ac7
gopalmunde <gopalmunde07@gmail.com>
2021-09-10 12:32:14 +0530

    add selenium basic programms

---
+++ parent.java
@@ -0,0 +1,27 @@
+package test;
+
+public class parent {
+           parent(){
+        	   System.out.println("Constructor default");
+        	   }
+     parent (int i) 
+     {
+    	 System.out.println("Constructor parameter int");
+     }     
+           
+      parent(String s,int i) 
+      {
+    System.out.println("Constructor parameter string");  
+      }    
+           
+      public void parent() {
+    	  System.out.println("parent");
+      }     
+	}
+
+
+	
+	
+	
+	
+
